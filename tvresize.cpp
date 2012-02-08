d2825d0bffdac3ccca76ad1978fe95d8c6e980a4
y-fujii <y-fujii@localhost>
2012-02-08 15:36:01 +0900

    switch to simple box filter

--- tvresize.cpp
+++ tvresize.cpp
@@ -15,30 +15,12 @@
 using namespace std;
 
 
-inline double hanning2( double x ) {
-	if( abs( x ) < 1.0 ) {
-		return 1.0 + cos( M_PI * x );
-	}
-	else {
-		return 0.0;
-	}
-}
-
-inline double sinc( double x ) {
-	if( abs( x ) <= numeric_limits<double>::min() ) {
-		return 1.0;
-	}
-	else {
-		return sin( M_PI * x ) / (M_PI * x);
-	}
-}
-
 struct Clip {
 	boost::gil::rgb8_pixel_t operator()( boost::gil::rgb32f_pixel_t src ) {
 		return boost::gil::rgb8_pixel_t(
-			min( max( int( src[0] * 255.0f + 0.5f ), 0 ), 255 ),
-			min( max( int( src[1] * 255.0f + 0.5f ), 0 ), 255 ),
-			min( max( int( src[2] * 255.0f + 0.5f ), 0 ), 255 )
+			int( min( max( src[0] * 255.0f + 0.5f, 0.0f ), 255.0f ) ),
+			int( min( max( src[1] * 255.0f + 0.5f, 0.0f ), 255.0f ) ),
+			int( min( max( src[2] * 255.0f + 0.5f, 0.0f ), 255.0f ) )
 		);
 	}
 };
@@ -81,7 +63,7 @@ struct TVMinimizer {
 };
 
 template<class SrcView, class OrgView, class DstView>
-void constrain( SrcView const& src, OrgView const& org, DstView& dst ) {
+void constrainBox( SrcView const& src, OrgView const& org, DstView const& dst ) {
 	using namespace boost::gil;
 	assert( src.dimensions() == dst.dimensions() );
 	assert( src.width() % org.width() == 0 );
@@ -90,51 +72,21 @@ void constrain( SrcView const& src, OrgView const& org, DstView& dst ) {
 	int const mw = src.width() / org.width();
 	int const mh = src.height() / org.height();
 
-	double sum = 0.0;
-	double kern[mw * 2][mh * 2];
-	for( int dy = 0; dy < mh * 2; ++dy ) {
-		for( int dx = 0; dx < mw * 2; ++dx ) {
-			double rx = double( dx - mw ) / mw;
-			double ry = double( dy - mh ) / mh;
-			//kern[dx][dy] = hanning2( max( abs( rx ), abs( ry ) ) );
-			kern[dx][dy] = hanning2( rx ) * hanning2( ry ) / (mw * mh * 4);
-			//kern[dx][dy] = hanning2( sqrt( rx * rx + ry * ry ) );
-			//kern[dx][dy] = sinc( rx ) * sinc( rx / 3.0 ) * sinc( ry ) * sinc( ry / 3.0 );
-			sum += kern[dx][dy];
-		}
-	}
-	/*
-	for( int dy = 0; dy < mh * 2; ++dy ) {
-		for( int dx = 0; dx < mw * 2; ++dx ) {
-			kern[dx][dy] /= sum;
-		}
-	}
-	*/
-
-	//copy_pixels( src, dst );
-	fill_pixels( dst, rgb32f_pixel_t( 0.0, 0.0, 0.0 ) );
-	for( int y = 0; y < org.height() - 2; ++y ) {
-		for( int x = 0; x < org.width() - 2; ++x ) {
+	for( int y = 0; y < org.height(); ++y ) {
+		for( int x = 0; x < org.width(); ++x ) {
 			for( int c = 0; c < num_channels<DstView>::value; ++c ) {
 				double s = 0.0;
-				for( int dy = 0; dy < mh * 2; ++dy ) {
-					for( int dx = 0; dx < mw * 2; ++dx ) {
-						s += kern[dx][dy] * src(x * mw + dx, y * mh + dy)[c];
+				for( int dy = 0; dy < mh; ++dy ) {
+					for( int dx = 0; dx < mw; ++dx ) {
+						s += src(x * mw + dx, y * mh + dy)[c];
 					}
 				}
-				double d = org(x, y)[c] - s;
-				for( int dy = 0; dy < mh * 2; ++dy ) {
-					for( int dx = 0; dx < mw * 2; ++dx ) {
-						dst(x * mw + dx, y * mh + dy)[c] += kern[dx][dy] * d;
+				double d = org(x, y)[c] - s * (1.0 / (mw * mh));
+				for( int dy = 0; dy < mh; ++dy ) {
+					for( int dx = 0; dx < mw; ++dx ) {
+						dst(x * mw + dx, y * mh + dy)[c] = src(x * mw + dx, y * mh + dy)[c] + d;
 					}
 				}
-			}
-		}
-	}
-	for( int y = 0; y < src.height(); ++y ) {
-		for( int x = 0; x < src.width(); ++x ) {
-			for( int c = 0; c < num_channels<DstView>::value; ++c ) {
-				dst(x, y)[c] += src(x, y)[c];
 			}
 		}
 	}
@@ -177,24 +129,59 @@ int main( int argc, char* const* argv ) {
 	try {
 		rgb32f_image_t src;
 		png_read_and_convert_image( srcFile, src );
-		rgb32f_image_t tmp0( src.dimensions() * scale );
-		rgb32f_image_t tmp1( src.dimensions() * scale );
-		rgb32f_view_t sub0 = subimage_view( view( tmp0 ), 1, 1, tmp0.width() - 2, tmp0.height() - 2 );
-		rgb32f_view_t sub1 = subimage_view( view( tmp1 ), 1, 1, tmp1.width() - 2, tmp1.height() - 2 );
+		int const zw = src.width() * scale;
+		int const zh = src.height() * scale;
+		rgb32f_image_t img0( zw + 2, zh + 2 );
+		rgb32f_image_t img1( zw + 2, zh + 2 );
+		rgb32f_view_t view0 = view( img0 );
+		rgb32f_view_t view1 = view( img1 );
 
-		generate_pixels( view( tmp0 ), RandomGray() );
+		generate_pixels( view0, RandomGray() );
 		for( int i = 0; i < maxCount; ++i ) {
 			cout << "\riter step: " << i;
 			cout.flush();
-			transform_pixel_positions( sub0, sub1,
+
+			transform_pixel_positions(
+				subimage_view( view0, 1, 1, zw, zh ),
+				subimage_view( view1, 1, 1, zw, zh ),
 				TVMinimizer<rgb32f_pixel_t, rgb32f_loc_t>()
 			);
-			constrain( view( tmp1 ), view( src ), view( tmp0 ) );
+			constrainBox(
+				subimage_view( view1, 1, 1, zw, zh ),
+				view( src ),
+				subimage_view( view0, 1, 1, zw, zh )
+			);
+			copy_pixels(
+				subimage_view( view0, 1, 1, zw, 1 ),
+				subimage_view( view0, 1, 0, zw, 1 )
+			);
+			copy_pixels(
+				subimage_view( view0, 1, zh    , zw, 1 ),
+				subimage_view( view0, 1, zh + 1, zw, 1 )
+			);
+			copy_pixels(
+				subimage_view( view0, 1, 1, 1, zh ),
+				subimage_view( view0, 0, 1, 1, zh )
+			);
+			copy_pixels(
+				subimage_view( view0, zw    , 1, 1, zh ),
+				subimage_view( view0, zw + 1, 1, 1, zh )
+			);
+			for( int c = 0; c < 3; ++c ) {
+				view0(     0,      0)[c] = view0( 1, 0)[c] + view0(0,  1)[c] - view0(     1,      1)[c];
+				view0(zw + 1,      0)[c] = view0(zw, 0)[c] + view0(0,  1)[c] - view0(zw + 1,      1)[c];
+				view0(     0, zh + 1)[c] = view0( 1, 0)[c] + view0(0, zh)[c] - view0(     1, zh + 1)[c];
+				view0(zw + 1, zh + 1)[c] = view0(zw, 0)[c] + view0(0, zh)[c] - view0(zw + 1, zh + 1)[c];
+			}
 		}
 		cout << endl;
 
-		rgb8_image_t dst( tmp0.dimensions() );
-		transform_pixels( view( tmp0 ), view( dst ), Clip() );
+		rgb8_image_t dst( zw, zh );
+		transform_pixels(
+			subimage_view( view0, 1, 1, zw, zh ),
+			view( dst ),
+			Clip()
+		);
 		png_write_view( dstFile, view( dst ) );
 	}
 	catch( exception const& e ) {
